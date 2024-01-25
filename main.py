82364aacfa90dca44efcaf095426aac73b2723c0
sam <samermatta2003@gmail.com>
2024-01-24 20:58:58 -0600

    main app

--- main.py
+++ main.py
@@ -1,8 +1,30 @@
-from fastapi import FastAPI
+
+import fastapi
+from fastapi.responses import RedirectResponse
+
+app = fastapi.FastAPI()
 
 
-app = FastAPI()
+
+info = dict()
+
 
 @app.get("/")
-async def root():
-    return {"message": "Hello"}
+async def read_root():
+    return {"message": "Hello, FastAPI!"}
+
+@app.get("/expired")
+async def exp():
+    return {"message": "subscription expired"}
+
+
+@app.get("/fetch-url/{name}/")
+async def fetch_url(name : str):
+    name = name.lower()
+    if name == "teki":
+        return RedirectResponse(url="/expired")
+    url_path = info.get(name, "Hello")
+    google_url = "https://www.google.com"
+    print("redirected")
+    return RedirectResponse(url=google_url)
+    # return {"message": f"Fetching data from URL: {url_path}"}
