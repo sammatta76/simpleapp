7bcc1548424e75d870043947e8c9828d7a10bc00
sam <samermatta2003@gmail.com>
2024-01-25 00:07:34 -0600

    validate

--- main.py
+++ main.py
@@ -19,12 +19,19 @@ async def exp():
 
 
 @app.get("/fetch-url/{name}/")
-async def fetch_url(name : str):
-    name = name.lower()
-    if name == "teki":
+async def fetch_url(name: str):
+    answer = validate(name)
+    if not answer:
         return RedirectResponse(url="/expired")
     url_path = info.get(name, "Hello")
     google_url = "https://www.google.com"
     print("redirected")
     return RedirectResponse(url=google_url)
     # return {"message": f"Fetching data from URL: {url_path}"}
+
+
+async def validate(name):
+    name = name.lower()
+    if name == "teki":
+        return False
+    return True
